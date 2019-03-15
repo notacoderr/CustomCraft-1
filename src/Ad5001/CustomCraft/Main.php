28f82444b53767b0711891b426c0a75f378d8ed2
Hanuko <40063996+YumikoMC@users.noreply.github.com>
2019-03-15 09:21:57 +0100

    Update Main.php

--- src/Ad5001/CustomCraft/Main.php
+++ src/Ad5001/CustomCraft/Main.php
@@ -10,19 +10,31 @@ class Main extends PluginBase{
 
 
    public function onEnable(){
-        foreach($this->getConfig()->getAll() as $craft) {
+
+        foreach($this->getCfg() as $craft) {
+
             $result = $this->getItem($craft["result"]);
-            $rec = new ShapedRecipe($result, ["abc","def","ghi"], [
-		    "a" => $this->getItem($craft["shape"][0][0]),
-		    "b" => $this->getItem($craft["shape"][0][1]),
-		    "c" => $this->getItem($craft["shape"][0][2]),
-		    "d" => $this->getItem($craft["shape"][1][0]),
-		    "e" => $this->getItem($craft["shape"][1][1]),
-		    "f" => $this->getItem($craft["shape"][1][2]),
-		    "g" => $this->getItem($craft["shape"][2][0]),
-		    "h" => $this->getItem($craft["shape"][2][1]),
-		    "i" => $this->getItem($craft["shape"][2][2]),
-	    ]);
+            $rec = new ShapedRecipe(
+                [
+                    "ABC",
+                    "DEF",
+                    "GHI",
+                ],
+                [
+                    "A" => $this->getItem($craft["shape"][0][0]),
+                    "B" => $this->getItem($craft["shape"][0][1]),
+                    "C" => $this->getItem($craft["shape"][0][2]),
+                    "D" => $this->getItem($craft["shape"][1][0]),
+                    "E" => $this->getItem($craft["shape"][1][1]),
+                    "F" => $this->getItem($craft["shape"][1][2]),
+                    "G" => $this->getItem($craft["shape"][2][0]),
+                    "H" => $this->getItem($craft["shape"][2][1]),
+                    "I" => $this->getItem($craft["shape"][2][2])
+                ],
+                [
+                    $result
+                ]
+                );
             $this->getServer()->getCraftingManager()->registerRecipe($rec);
             $this->getLogger()->info("Registered recipe for " . $this->getItem($craft["result"])->getName());
         }
@@ -36,26 +48,31 @@ class Main extends PluginBase{
     
     
     public function getItem(array $item) : Item {
-        $result = Item::fromString($item[0]);
+        $result = Item::get($item[0]);
         if(isset($item[1])) {
-        	$result->setCount((int) $item[1]);
+            $result->setCount($item[1]);
         }
         if(isset($item[2])) {
             $tags = $exception = null;
-            $data = $item[2];
-			try{
-				$tags = JsonNBTParser::parseJSON($data);
-			}catch (\Throwable $ex){
-				$exception = $ex;
-			}
+			$data = $item[2];
+            try{
+                $tags = JsonNbtParser::parseJson($data);
+            }catch (\Throwable $ex){
+                $exception = $ex;
+            }
 
-			if(!($tags instanceof \pocketmine\nbt\tag\CompoundTag) or $exception !== null){
-				$this->getLogger()->warning("Could not get item [" . implode(", ", $item) . "] : " . $exception->getMessage());
-				return $result;
-			}
+            if(!($tags instanceof \pocketmine\nbt\tag\CompoundTag) or $exception !== null){
+                $this->getLogger()->warning(new TranslationContainer("commands.give.tagError", [$exception !== null ? $exception->getMessage() : "Invalid tag conversion"]));
+                return $result;
+            }
             
             $result->setNamedTag($tags);
         }
         return $result;
+    }
+    
+    
+    private function getCfg() {
+        return yaml_parse(file_get_contents($this->getDataFolder() . "config.yml"));
     }
 }
